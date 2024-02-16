7d611e86b112b74df8621737f3e0a2b6e2dbea5d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2024-02-16 20:20:29 +0100

    Revert "Bump down to intended PHP8.1 for initial release"

--- src/RunTestsInFibersTrait.php
+++ src/RunTestsInFibersTrait.php
@@ -17,6 +17,8 @@ use function React\Promise\Timer\sleep;
 
 trait RunTestsInFibersTrait
 {
+    private const DEFAULT_TIMEOUT_SECONDS = 30;
+
     private string|null $realTestName = null;
 
     /** @codeCoverageIgnore Invoked before code coverage data is being collected. */
@@ -45,7 +47,7 @@ trait RunTestsInFibersTrait
 
         assert(is_string($this->realTestName));
 
-        $timeout         = 60;
+        $timeout         = self::DEFAULT_TIMEOUT_SECONDS;
         $reflectionClass = new ReflectionClass($this::class);
         foreach ($reflectionClass->getAttributes() as $classAttribute) {
             $classTimeout = $classAttribute->newInstance();
--- src/TimeOut.php
+++ src/TimeOut.php
@@ -7,10 +7,10 @@ namespace WyriHaximus\React\PHPUnit;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
-final class TimeOut implements TimeOutInterface
+final readonly class TimeOut implements TimeOutInterface
 {
     public function __construct(
-        private readonly int|float $timeout,
+        private int|float $timeout,
     ) {
     }
 
